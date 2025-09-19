5a88a179ae454fb77f375a458841d26699b5f7db
miqr-dev <ara.matoyan@miqr.de>
2025-09-19 10:28:38 +0200

    fix the answer for Avem

--- resources/js/components/AvemResult.vue
+++ resources/js/components/AvemResult.vue
@@ -16,7 +16,10 @@ import { AVEM_QUESTIONS } from '@/pages/Questions/AVEMQuestions'
 
 Chart.register(LineElement, PointElement, LinearScale, CategoryScale, Tooltip, Legend, Title, annotationPlugin)
 
-const props = defineProps<{ results: any }>()
+const props = defineProps<{
+  results: any;
+  showAnswers: boolean;
+}>();
 
 const scaleLabels = [
   'Subjektive Bedeutsamkeit der Arbeit',
@@ -266,7 +269,7 @@ const detailRows = computed(() => {
       <Line :data="chartData" :options="chartOptions" />
     </div>
 
-    <details v-if="detailRows.length" class="mt-2">
+    <details v-if="showAnswers && detailRows.length" class="mt-2">
       <summary class="cursor-pointer select-none px-2 py-1 bg-muted/40 rounded">Antworten</summary>
       <div class="overflow-x-auto mt-3">
         <table class="min-w-full text-sm border rounded shadow">
