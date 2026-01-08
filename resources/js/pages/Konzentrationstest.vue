edc6d5ccccb92cedb7476cf5583161f4bbf66170
miqr-dev <84575963+miqr-dev@users.noreply.github.com>
2026-01-08 16:00:41 +0100

    Adjust example gap in Paching

--- resources/js/pages/Konzentrationstest.vue
+++ resources/js/pages/Konzentrationstest.vue
@@ -136,9 +136,9 @@ const exampleOriginalRow: { parts: RowParts } = {
   parts: ['Sylvia Müller', '45678 Pasching', 'Leipziger Str. 4', 'Tel. 0212 7874624'],
 }
 const exampleCopyRow: { parts: RowParts } = {
-  parts: ['Silvya Müller', '45678 P aching', 'Laibziger Str. 4', 'Tel. 0122 787462-'],
+  parts: ['Silvya Müller', '45678 Pa ching', 'Laibziger Str. 4', 'Tel. 0122 787462-'],
 }
-const exampleMarked: number[][] = [[1, 4], [7], [1], [6, 7, 16]]
+const exampleMarked: number[][] = [[1, 4], [8], [1], [6, 7, 16]]
 const getExampleOriginalChars = (partIndex: number) => Array.from(exampleOriginalRow.parts[partIndex])
 const getExampleCopyChars = (partIndex: number) => Array.from(exampleCopyRow.parts[partIndex])
 
