7f77262a5dc84807d42b138eda5ce31f4661ffd7
miqr-dev <84575963+miqr-dev@users.noreply.github.com>
2026-04-14 14:26:23 +0200

    Fix BT page setup by restoring computed import

--- resources/js/pages/BT.vue
+++ resources/js/pages/BT.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
 import { Head } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const emit = defineEmits(['started']);
 
